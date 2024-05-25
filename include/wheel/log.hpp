02e0d0c490ae7cf9b66d29404b6762d0e08c6afc
m1dsolo <yx1053532442@gmail.com>
2024-05-25 21:18:26 +0800

    feat(Log): can only log one value

--- include/wheel/log.hpp
+++ include/wheel/log.hpp
@@ -30,31 +30,39 @@ private:
 public:
     template <typename ...Args>
     static void log(LogLevel level, with_source_location_<std::format_string<Args...>> fmt, Args&& ...args);
-
     template <typename ...Args>
     static void debug(with_source_location_<std::format_string<Args...>> fmt, Args&& ...args) {
         return log(LogLevel::DEBUG, fmt, std::forward<Args>(args)...);
     }
-
     template <typename ...Args>
     static void info(with_source_location_<std::format_string<Args...>> fmt, Args&& ...args) {
         return log(LogLevel::INFO, fmt, std::forward<Args>(args)...);
     }
-
     template <typename ...Args>
     static void warning(with_source_location_<std::format_string<Args...>> fmt, Args&& ...args) {
         return log(LogLevel::WARNING, fmt, std::forward<Args>(args)...);
     }
-
     template <typename ...Args>
     static void error(with_source_location_<std::format_string<Args...>> fmt, Args&& ...args) {
         return log(LogLevel::ERROR, fmt, std::forward<Args>(args)...);
     }
-
     template <typename ...Args>
     static void fatal(with_source_location_<std::format_string<Args...>> fmt, Args&& ...args) {
         return log(LogLevel::FATAL, fmt, std::forward<Args>(args)...);
     }
+
+    template <typename T>
+    static void log(LogLevel level, T& val, std::source_location loc = std::source_location::current());
+    template <typename T>
+    static void debug(T& val) { return log(LogLevel::DEBUG, val); }
+    template <typename T>
+    static void info(T& val) { return log(LogLevel::INFO, val); }
+    template <typename T>
+    static void warning(T& val) { return log(LogLevel::WARNING, val); }
+    template <typename T>
+    static void error(T& val) { return log(LogLevel::ERROR, val); }
+    template <typename T>
+    static void fatal(T& val) { return log(LogLevel::FATAL, val); }
 
     static void set_log_level(LogLevel level) { max_log_level_ = level; }
     static void set_log_file(std::string_view s) { log_file_.open(s.data(), std::ios::app); }
@@ -68,10 +76,13 @@ private:
             : inner(std::forward<U>(u)), loc(loc) {}
         constexpr const T& format() const { return inner; }
         constexpr const std::source_location& location() const { return loc; }
+
     private:
         T inner;
         std::source_location loc;
     };
+
+    static void log_(LogLevel level, std::string_view sv, const std::source_location& loc);
 
     [[nodiscard]]
     static std::string add_color_to_string_(LogLevel level, const std::string& s, std::string_view color);
@@ -90,24 +101,12 @@ private:
 template <typename ...Args>
 void Log::log(LogLevel level, with_source_location_<std::format_string<Args...>> fmt, Args&& ...args) {
     const auto& loc = fmt.location();
-    std::chrono::zoned_time now{
-        std::chrono::current_zone(),
-        std::chrono::time_point_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now())
-    };
+    return log_(level, std::vformat(fmt.format().get(), std::make_format_args(args...)), loc);
+}
 
-    auto msg = std::format("{} [{}] {}:{} {}", 
-                             now,
-                             Enum::enum2str<LogLevel, 0, 4>(level),
-                             loc.file_name(),
-                             loc.line(),
-                             std::vformat(fmt.format().get(), std::make_format_args(args...))
-                           );
-    if (level >= max_log_level_) {
-        std::cout << add_color_to_string_(level, msg, level_ansi_color_[(uint8_t)level]) << std::endl;
-    }
-    if (log_file_.is_open()) {
-        log_file_ << msg << std::endl;
-    }
+template <typename T>
+void Log::log(LogLevel level, T& val, std::source_location loc) {
+    return log_(level, std::format("{}", val), loc);
 }
 
 }  // namespace wheel
--- src/log.cpp
+++ src/log.cpp
@@ -17,6 +17,27 @@ std::ofstream Log::log_file_ = []() {
     return std::ofstream{};
 } ();
 
+void Log::log_(LogLevel level, std::string_view sv, const std::source_location& loc) {
+    std::chrono::zoned_time now{
+        std::chrono::current_zone(),
+        std::chrono::time_point_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now())
+    };
+
+    auto msg = std::format("{} [{}] {}:{} {}", 
+                             now,
+                             Enum::enum2str<LogLevel, 0, 4>(level),
+                             loc.file_name(),
+                             loc.line(),
+                             sv
+                           );
+    if (level >= max_log_level_) {
+        std::cout << add_color_to_string_(level, msg, level_ansi_color_[(uint8_t)level]) << std::endl;
+    }
+    if (log_file_.is_open()) {
+        log_file_ << msg << std::endl;
+    }
+}
+
 std::string Log::add_color_to_string_(LogLevel level, const std::string& s, std::string_view color) {
     return std::format("\E[{}{}\E[m", color, s);
 }
