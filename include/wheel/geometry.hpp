185011f205c70f39aeace21485ac6d67716ca225
m1dsolo <yx1053532442@gmail.com>
2024-08-05 21:18:38 +0800

    feat: update Rect

--- include/wheel/geometry.hpp
+++ include/wheel/geometry.hpp
@@ -45,29 +45,51 @@ struct Vector2D {
 
 template <typename T>
 struct Rect {
-    Vector2D<T> position;
-    Vector2D<T> size;
+    Rect() {}
+    Rect(T x0, T y0, T x1, T y1) : x0(x0), y0(y0), x1(x1), y1(y1) {}
+    Rect(const Vector2D<T>& position, const Vector2D<T>& size) {
+        x0 = position.x - size.x / 2;
+        y0 = position.y - size.y / 2;
+        x1 = position.x + size.x / 2;
+        y1 = position.y + size.y / 2;
+    }
 
     friend std::ostream& operator<<(std::ostream& os, const Rect& rect) {
-        return os << "position: " << rect.position << ", size: " << rect.size;
+        return os << "(" << rect.x0 << ", " << rect.y0 << ", " << rect.x1 << ", " << rect.y1 << ")";
     }
 
     T intersection(const Rect<T>& other) {
-        T x1 = std::max(position.x - size.x / 2, other.position.x - other.size.x / 2);
-        T y1 = std::max(position.y - size.y / 2, other.position.y - other.size.y / 2);
-        T x2 = std::min(position.x + size.x / 2, other.position.x + other.size.x / 2);
-        T y2 = std::min(position.y + size.y / 2, other.position.y + other.size.y / 2);
-        if (x2 < x1 || y2 < y1) {
-            return 0;
+        T x0 = std::max(this->x0, other.x0);
+        T y0 = std::max(this->y0, other.y0);
+        T x1 = std::min(this->x1, other.x1);
+        T y1 = std::min(this->y1, other.y1);
+        if (x0 < x1 && y0 < y1) {
+            return (x1 - x0) * (y1 - y0);
         }
-        return (x2 - x1) * (y2 - y1);
+        return 0;
+    }
+
+    bool contains(const Vector2D<T>& point) const {
+        return x0 <= point.x && point.x <= x1 && y0 <= point.y && point.y <= y1;
+    }
+    
+    bool is_overlapping(const Rect<T>& other) const {
+        return x0 < other.x1 && x1 > other.x0 && y0 < other.y1 && y1 > other.y0;
     }
 
     template <typename U>
-    bool intersection(const Vector2D<U>& pos) {
-        return pos.x >= position.x - size.x / 2 && pos.x <= position.x + size.x / 2 &&
-               pos.y >= position.y - size.y / 2 && pos.y <= position.y + size.y / 2;
+    operator Rect<U>() const {
+        return {static_cast<U>(x0), static_cast<U>(y0), static_cast<U>(x1), static_cast<U>(y1)};
     }
+
+    std::pair<T, T> center() const { return {(x0 + x1) / 2, (y0 + y1) / 2}; }
+    std::pair<T, T> size() const { return {x1 - x0, y1 - y0}; } 
+
+    T x0 = 0;
+    T y0 = 0;
+    T x1 = 0;
+    T y1 = 0;
+
 };
 
 }  // namespace wheel
